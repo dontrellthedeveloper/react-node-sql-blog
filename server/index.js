2fe3dc078477c97f909c67115033ee0e2bdb5123
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-09-06 23:32:36 -0400

    added a post request to create a post and a get request to get all of the posts

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ app.use(express.json());
 
 const db = require("./models");
 
+// Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+
 
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
