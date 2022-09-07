a1486a76bba8ed4af7fc3c2fa62c449cc94d621a
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-09-06 22:54:44 -0400

    added node server and connected to sql database with sequelize

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const db = require("./models");
+
+
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => {
+        console.log("Server running on port 3001");
+    });
+});
